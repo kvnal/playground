0dba376f6dc66841c95c0c87903d54b02cac1a96
kvnal <72142540+kvnal@users.noreply.github.com>
2024-04-09 22:16:51 +0530

    test gyro

--- orientation/index.js
+++ orientation/index.js
@@ -90,3 +90,17 @@ function getInnerHTML(alpha, beta, gamma){
 
 
 // update del
+
+
+let gyroscope = new Gyroscope({ frequency: 60 });
+
+gyroscope.addEventListener("reading", (e) => {
+  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+
+  document.getElementById("gyro").innerText = `x: ${gyroscope.x}\ny: ${gyroscope.y}\nz:${gyroscope.z}`
+
+});
+
+gyroscope.start();
